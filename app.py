ce1394da580c096f12577834284604702dc28c06
Bom <pongchaihantapong@protonmail.com>
2023-01-20 13:04:32 +0700

    Add docstring

--- app.py
+++ app.py
@@ -13,12 +13,40 @@ db = SQLAlchemy(app)
 
 
 class VendingMachine(db.Model):
+    """
+    A Class to represent Vending Machine Object
+
+    Attributes
+    ----------
+    id : int
+        the identification of each Vending Machine
+    name: String
+        name of each Vending Machine
+    location: String
+        location of each Vending Machine
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     location = db.Column(db.String(50))
 
 
 class Item(db.Model):
+    """
+    A Class to represent Item
+
+    Attributes
+    ----------
+    id : int
+        the identification of each Item
+    name: String
+        name of each Item
+    price: Float
+        price of each Item
+    quantity: Int
+        quantity of each Item
+    vending_machine_id : vending_machine.id
+        link of vending machine id where relationship with the item
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     price = db.Column(db.Float)
@@ -30,6 +58,7 @@ class Item(db.Model):
 VendingMachine.items = db.relationship("Item", order_by=Item.id, back_populates="vending_machine")
 
 
+# Create a vending machine
 @app.route('/create_vm', methods=['POST'])
 def create_vm():
     name = request.json['name']
@@ -40,6 +69,7 @@ def create_vm():
     return jsonify({'message': 'Vending Machine created successfully'})
 
 
+# Delete vending machine
 @app.route('/delete_vm/<int:id>', methods=['DELETE'])
 def delete_vm(id):
     vm = VendingMachine.query.get(id)
@@ -48,6 +78,7 @@ def delete_vm(id):
     return jsonify({'message': 'Vending Machine deleted successfully'})
 
 
+# Create item in vending machine
 @app.route('/create_item/<int:vm_id>', methods=['POST'])
 def create_item(vm_id):
     name = request.json['name']
@@ -60,6 +91,7 @@ def create_item(vm_id):
     return jsonify({'message': 'Item created successfully'})
 
 
+# Add product to the vending machine
 @app.route('/add_item_to_stock/<int:vm_id>/<int:item_id>', methods=['PUT'])
 def add_item_to_stock(vm_id, item_id):
     quantity = request.json['quantity']
@@ -76,6 +108,7 @@ def add_item_to_stock(vm_id, item_id):
         return jsonify({'message': 'Vending Machine not found'}), 404
 
 
+# Edit stock
 @app.route('/edit_item_stock/<int:vm_id>/<int:item_id>', methods=['PUT'])
 def edit_item_stock(vm_id, item_id):
     quantity = request.json['quantity']
@@ -92,6 +125,7 @@ def edit_item_stock(vm_id, item_id):
         return jsonify({'message': 'Vending Machine not found'}), 404
 
 
+# Remove item
 @app.route('/remove_item_from_stock/<int:vm_id>/<int:item_id>', methods=['PUT'])
 def remove_item_from_stock(vm_id, item_id):
     quantity = request.json['quantity']
@@ -111,6 +145,7 @@ def remove_item_from_stock(vm_id, item_id):
         return jsonify({"error": "Vending machine not found"}), 404
 
 
+# Get Item in Vending Machine
 @app.route('/get_vending_machines_items/<int:vm_id>', methods=['GET'])
 def get_stock_by_vm(vm_id):
     vm = VendingMachine.query.get(vm_id)
@@ -124,6 +159,7 @@ def get_stock_by_vm(vm_id):
         return jsonify({'message': 'Vending Machine not found, please call the correct ID'})
 
 
+# Get all Vending Machine
 @app.route('/get_vending_machines', methods=['GET'])
 def view_vm():
     vending_machines = VendingMachine.query.all()
@@ -136,4 +172,3 @@ def view_vm():
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
-
