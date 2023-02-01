a2aa16647a7069d0d80bbb6cf689b9bdd0730e30
Bom <pongchaihantapong@protonmail.com>
2023-02-01 20:42:23 +0700

    Installed Poetry, added dependencies such as flake8, pytest and etc for test handling system

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy() # Create a SQLAlchemy instance
+db = SQLAlchemy()  # Create a SQLAlchemy instance
+
 
 # Create a VendingMachine model
 class VendingMachine(db.Model):
@@ -17,6 +18,7 @@ class VendingMachine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     location = db.Column(db.String(50))
+
 
 # Create an Item model
 class Item(db.Model):
@@ -39,4 +41,5 @@ class Item(db.Model):
     vending_machine = db.relationship("VendingMachine", back_populates="items")
 
 
-VendingMachine.items = db.relationship("Item", order_by=Item.id, back_populates="vending_machine") # Create a relationship between the VendingMachine and Item models
+VendingMachine.items = db.relationship("Item", order_by=Item.id,
+                                       back_populates="vending_machine")  # Create a relationship between the VendingMachine and Item models
